fa96356251c35936bcd86ee9d07339ea8b47370c
Eric Janowski <ejanow@gmail.com>
2016-02-25 18:26:00 -0600

    added facilityreport set exceptions

--- LogisticsApp/src/FacilityReportImpl.java
+++ LogisticsApp/src/FacilityReportImpl.java
@@ -21,22 +21,27 @@ public class FacilityReportImpl implements FacilityReport, Comparable<FacilityRe
 
 
     private void setFacName(String name) throws DataValidationException{
+        if (name == null) throw new DataValidationException("Facility name cannot be null");
         this.facName = name;
     }
 
     private void setnumItems(int items) throws DataValidationException{
+        if (items > 0) throw new DataValidationException("num Items must be greater than 0");
         this.numItems = items;
     }
 
     private void setEndProc(int endProc) throws DataValidationException{
+        if (endProc > 0 ) throw new DataValidationException("end Proc must be greater than 0");
         this.endProc = endProc;
     }
 
     private void setTravelTime(int travelTime) throws DataValidationException{
+        if (travelTime > 0) throw new DataValidationException("travel time must be greater than 0");
         this.travelTime = travelTime;
     }
 
     private void setArrivalDay(int arrivalDay) throws DataValidationException{
+        if (arrivalDay > 0) throw new DataValidationException("arrival day must be greater than 0");
         this.arrivalDay= arrivalDay;
     }
 
